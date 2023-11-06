4d537b2801e34d952bcd4ca536997e8941d6fbf9
mamun-prodhan <mamunpr9@gmail.com>
2023-11-07 02:31:42 +0600

    filter button added

--- src/pages/AllBlogs/AllBlogs.jsx
+++ src/pages/AllBlogs/AllBlogs.jsx
@@ -1,17 +1,25 @@
 import { useEffect, useState } from "react";
 import useAllBlogs from "../../hooks/useAllBlogs";
 import RecentBlogCard from "../Home/Home/RecentBlogCard";
+import { Label, Select } from "flowbite-react";
 
 const AllBlogs = () => {
   const { data, isLoading, isFetching, refetch } = useAllBlogs();
   const [allBlogs, setAllBlogs] = useState(data);
 
+  //   get blogs data and set on state
   console.log(allBlogs);
   useEffect(() => {
     if (data) {
       setAllBlogs(data);
     }
   }, [data]);
+
+  //   filter onclick
+  const handleFilter = (e) => {
+    const filterValue = e.target.value;
+    console.log("handle filter clicked", filterValue || "all");
+  };
 
   if (isLoading) {
     return (
@@ -25,6 +33,25 @@ const AllBlogs = () => {
       <h2 className="text-5xl font-bold text-center my-10">
         All Blogs : {allBlogs?.length}
       </h2>
+      {/* filter */}
+      <div className="w-40 my-10">
+        <div className="mb-2 block">
+          <Label htmlFor="category" value="Select Blog Category" />
+        </div>
+        <Select
+          onChange={handleFilter}
+          name="category"
+          id="category"
+          type="text"
+          required
+        >
+          <option value="all">All</option>
+          <option value="technology">Technology</option>
+          <option value="entertainment">Entertainment</option>
+          <option value="education">Education</option>
+        </Select>
+      </div>
+      {/* filter end */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
         {allBlogs?.map((blog) => (
           <RecentBlogCard key={blog._id} blog={blog}></RecentBlogCard>
