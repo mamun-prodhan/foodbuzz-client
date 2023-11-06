108cde3acc75d718434317249d65141f913b880d
mamun-prodhan <mamunpr9@gmail.com>
2023-11-06 11:26:51 +0600

    firebase configured and .env.local file implemented

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,6 +1,8 @@
 import { createContext } from "react";
-
+import app from "../firebase/firebase.config";
+import { getAuth } from "firebase/auth";
 export const AuthContext = createContext(null);
+const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
   const authInfo = {
