2c627e912e6adf23d2cb876a04f72418a5bdd79c
mamun-prodhan <mamunpr9@gmail.com>
2023-11-07 08:23:52 +0600

    filter implemented in all blogs page

--- src/hooks/useAllBlogs.jsx
+++ src/hooks/useAllBlogs.jsx
@@ -1,11 +1,21 @@
 import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
-const useAllBlogs = () => {
+const useAllBlogs = (selectedCategory, search) => {
+  console.log("useAllBlogs", selectedCategory);
   const { data, isLoading, isFetching, refetch } = useQuery({
     queryKey: ["allBlogs"],
     queryFn: async () => {
-      const data = await fetch("http://localhost:5000/allblogs");
-      return await data.json();
+      try {
+        const res = await axios.post("http://localhost:5000/allblogs", {
+          selectedCategory,
+          search,
+        });
+        return res.data;
+      } catch (error) {
+        console.log(error);
+        throw new Error(error.message);
+      }
     },
   });
   return { data, isLoading, isFetching, refetch };
--- src/pages/AllBlogs/AllBlogs.jsx
+++ src/pages/AllBlogs/AllBlogs.jsx
@@ -4,22 +4,31 @@ import RecentBlogCard from "../Home/Home/RecentBlogCard";
 import { Label, Select } from "flowbite-react";
 
 const AllBlogs = () => {
-  const { data, isLoading, isFetching, refetch } = useAllBlogs();
-  const [allBlogs, setAllBlogs] = useState(data);
+  const [selectedCategory, setSelectedCategory] = useState("all");
+  const { data, isLoading, isFetching, refetch } =
+    useAllBlogs(selectedCategory);
+
+  console.log("selected category", selectedCategory);
+  console.log(`http://localhost:5000/allblogs/${selectedCategory}`);
 
   //   get blogs data and set on state
-  console.log(allBlogs);
-  useEffect(() => {
-    if (data) {
-      setAllBlogs(data);
-    }
-  }, [data]);
+  //   console.log(allBlogs);
+  //   useEffect(() => {
+  //     if (data) {
+  //       setAllBlogs(data);
+  //     }
+  //   }, [data]);
 
   //   filter onclick
   const handleFilter = (e) => {
     const filterValue = e.target.value;
-    console.log("handle filter clicked", filterValue || "all");
+    setSelectedCategory(filterValue);
+    console.log("handle filter clicked", filterValue);
   };
+
+  useEffect(() => {
+    refetch();
+  }, [selectedCategory]);
 
   if (isLoading) {
     return (
@@ -28,11 +37,19 @@ const AllBlogs = () => {
       </h2>
     );
   }
+  //   if (isFetching) {
+  //     return (
+  //       <h2 className="text-5xl font-bold text-red-600 text-center my-20">
+  //         Loading
+  //       </h2>
+  //     );
+  //   }
   return (
     <div className="max-w-7xl mx-auto">
       <h2 className="text-5xl font-bold text-center my-10">
-        All Blogs : {allBlogs?.length}
+        All Blogs : {data?.length}
       </h2>
+      {/* search implement */}
       {/* filter */}
       <div className="w-40 my-10">
         <div className="mb-2 block">
@@ -53,7 +70,7 @@ const AllBlogs = () => {
       </div>
       {/* filter end */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-        {allBlogs?.map((blog) => (
+        {data?.map((blog) => (
           <RecentBlogCard key={blog._id} blog={blog}></RecentBlogCard>
         ))}
       </div>
